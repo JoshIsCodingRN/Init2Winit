1adc41016c3b9a91250b8a469754156cdff51398
Joshua Howard <howardajoshua@gmail.com>
2025-04-12 16:17:31 -0400

    ascii saves as png

--- backend/ascii.py
+++ backend/ascii.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 import sys
+from PIL import Image, ImageDraw, ImageFont
 
 
 img = cv.imread(r"C:\Users\howar\Downloads\vegeta.jpg", cv.IMREAD_GRAYSCALE)
@@ -18,23 +19,35 @@ while (height > 640 or width > 500):
 print(height, "\n")
 print(width)
 
-
+#Gaussian filtering
+blur = cv.GaussianBlur(img,(5,5),0)
+img = blur
 
 strung = r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,^`'."
 finalString = ""
 
 
 
-with open("output.txt", "w") as f:
-    sys.stdout = f
+#with open("output.txt", "w") as f:
+#   sys.stdout = f
+image = Image.new('RGB', (width * 12, height * 12), (0, 0 , 0))
 
-    for i in range(height):
-        for j in range(width):
-            intensity = img[i, j]
-            #print(intensity)
-            finalString += strung[int(intensity / 3.80)] + " "
-        f.write(finalString + "\n")
-        finalString = ""
-        
-f.close()           
+draw = ImageDraw.Draw(image)
+
+for i in range(height):
+    for j in range(width):
+        intensity = img[i, j]
+        #print(intensity)
+        finalString += strung[int(intensity / 3.80)] + " "
+    #f.write(finalString + "\n")
+    draw.text((0,i * 12), finalString + "\n", font = ImageFont.truetype("consola.ttf"))
+    finalString = ""
+
+image.show()
+image.save("frame.jpg")
+#f.close()           
+
+
+
+
 
